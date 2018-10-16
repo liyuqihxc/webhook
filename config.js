0ab7bfd498d18a43d856ead032c86821a5abaf41
liyuqihxc <liyuqihxc@gmail.com>
2018-10-16 17:57:22 +0800

    PSMS

--- config.js
+++ config.js
@@ -10,16 +10,9 @@ var app_path = '/autodeploy'
 // 项目配置
 var projects = [
     {
-        name: 'liyuqihxc.top',
-        path: '/www/liyuqihxc.top',
-        url: 'https://github.com/liyuqihxc/liyuqihxc.top.git',//不用ssh可以省去设置ssh key的麻烦
-        exec: './shell/blog/blog_deploy.sh',
-        branch: 'develop'// default: master
-    },
-    {
         name: 'PSMS',
         path: 'D:\\PSMS',
-        url: 'http://192.168.0.250:9090/liyuqi/PSMS.git',
+        url: 'git@192.168.0.250:liyuqi/PSMS.git',
         exec: 'run-deploy.cmd',
         branch: 'develop'
     }
